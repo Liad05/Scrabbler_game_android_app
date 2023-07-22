bbe597f4ed0f0669fc63578dc85e687e34d4f3ec
Raff Desper <liadgam10@gmail.com>
2023-07-22 11:57:19 +0300

    all the files so far.

---
+++ app/src/main/java/com/example/scrabbler_game/MainActivity.kt
@@ -0,0 +1,70 @@
+package com.example.scrabbler_game
+
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var gameIdEntry: EditText
+    private lateinit var errorLabel: TextView
+    private lateinit var submitButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        gameIdEntry = findViewById(R.id.game_id_entry)
+        errorLabel = findViewById(R.id.error_label)
+        submitButton = findViewById(R.id.submit_button)
+
+        submitButton.setOnClickListener {
+            submit()
+        }
+    }
+
+    private fun submit() {
+        val gameId = gameIdEntry.text.toString()
+        val serverIP = "" //enter your server IP here
+        val urlString = "http://$serverIP/test_tomcat_4_war_exploded/api/hello-world/getStateById?ID=$gameId"
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val url = URL(urlString)
+                val urlConnection = url.openConnection() as HttpURLConnection
+                urlConnection.requestMethod = "GET"
+                val responseCode = urlConnection.responseCode
+
+                if(responseCode == 200){
+                    val inReader = BufferedReader(InputStreamReader(urlConnection.inputStream))
+                    val response = StringBuilder()
+
+                    inReader.use { it.readLines().forEach { response.append(it) } }
+
+                    // Parse the response as needed
+                    // Use Dispatchers.Main to update the UI
+
+                } else {
+                    runOnUiThread {
+                        errorLabel.text = "No game with this ID"
+                    }
+                }
+
+                urlConnection.disconnect()
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
